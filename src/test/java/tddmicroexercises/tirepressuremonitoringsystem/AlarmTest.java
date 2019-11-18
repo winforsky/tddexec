d466ea001b9c633ee7d6c1c266f513c73da4d527
郑少朋 <911917@nd.com>
2019-11-18 11:27:30 +0800

    增加新特性说明

--- src/test/java/tddmicroexercises/tirepressuremonitoringsystem/AlarmTest.java
+++ src/test/java/tddmicroexercises/tirepressuremonitoringsystem/AlarmTest.java
@@ -8,6 +8,8 @@ public class AlarmTest {
     @Test
     public void testcheck() throws Exception {
         assertEquals(6, 2+4);
+        //TODO:实现新特性：若前一次检查的胎压值在正常范围之外并保健，后一次检查的胎压值又回到正常范围，此时需要停止报警。
+
     }
 
 }
