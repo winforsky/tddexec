e86bec5a339acb7951722e2d80cbd9d17b0e9c50
郑少朋 <911917@nd.com>
2019-11-18 15:34:11 +0800

    发现问题，并补充todo特性

--- TurnTicketDispenser/src/main/java/tddmicroexercises/turnticketdispenser/TicketDispenser.java
+++ TurnTicketDispenser/src/main/java/tddmicroexercises/turnticketdispenser/TicketDispenser.java
@@ -2,9 +2,12 @@ package tddmicroexercises.turnticketdispenser;
 
 public class TicketDispenser
 {
+    //TODO:拟新增特性：VIP客户的票号从1001开始
+    //TODO:拟新增特性：普通客户的票号从2001开始
 
     public TurnTicket getTurnTicket()
     {
+        //TODO: 属于针对具体实现编程，不是针对抽象编程，违反依赖倒置和开闭原则
         int newTurnNumber = TurnNumberSequence.getNextTurnNumber();
         TurnTicket newTurnTicket = new TurnTicket(newTurnNumber);
 
