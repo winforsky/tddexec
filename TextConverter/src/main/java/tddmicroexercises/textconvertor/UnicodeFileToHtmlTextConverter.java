356521f034ed8641b923f4a7a86a8d64f8e44bc7
郑少朋 <911917@nd.com>
2019-11-18 18:45:45 +0800

    通过面向接口编程去掉对文件系统的依赖

--- TextConverter/src/main/java/tddmicroexercises/textconvertor/UnicodeFileToHtmlTextConverter.java
+++ TextConverter/src/main/java/tddmicroexercises/textconvertor/UnicodeFileToHtmlTextConverter.java
@@ -2,17 +2,23 @@ package tddmicroexercises.textconvertor;
 
 import org.apache.commons.lang3.StringEscapeUtils;
 
-import java.io.BufferedReader;
-import java.io.FileReader;
-import java.io.IOException;
+import java.io.*;
 
 public class UnicodeFileToHtmlTextConverter
 {
-    private String fullFilenameWithPath;
+//    private String fullFilenameWithPath;
+    private Reader reader;
 
-    public UnicodeFileToHtmlTextConverter(String fullFilenameWithPath)
-    {
-        this.fullFilenameWithPath = fullFilenameWithPath;
+    public UnicodeFileToHtmlTextConverter(String fullFilenameWithPath) throws FileNotFoundException {
+//        this.fullFilenameWithPath = fullFilenameWithPath;
+        this.reader = new FileReader(fullFilenameWithPath);
+    }
+
+    public UnicodeFileToHtmlTextConverter(Reader reader) {
+        this.reader = reader;
+    }
+
+    public UnicodeFileToHtmlTextConverter() {
     }
 
     //TODO:新增特性：将一个unicode的纯文本字符串转化为HTML编码的字符串
@@ -22,7 +28,8 @@ public class UnicodeFileToHtmlTextConverter
     public String convertToHtml() throws IOException
     {
         //TODO:依赖具体实现进行的编程，需要改成面向接口的编程，否则违反依赖倒置和开闭原则
-        try (BufferedReader reader = new BufferedReader(new FileReader(fullFilenameWithPath)))
+//
+        try (BufferedReader reader = new BufferedReader(this.reader))
             {
 
             String line = reader.readLine();
--- TextConverter/src/test/java/tddmicroexercises/textconvertor/UnicodeFileToHtmlTextConverterTest.java
+++ TextConverter/src/test/java/tddmicroexercises/textconvertor/UnicodeFileToHtmlTextConverterTest.java
@@ -7,6 +7,7 @@ import org.junit.Test;
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
+import java.io.StringReader;
 
 public class UnicodeFileToHtmlTextConverterTest
 {
@@ -14,5 +15,18 @@ public class UnicodeFileToHtmlTextConverterTest
     @Test
     public void testConvert(){
         Assert.assertEquals(3, 1+2);
+    }
+
+    @Test
+    public void should_convert_empersand() throws IOException {
+
+        //Arrange
+        UnicodeFileToHtmlTextConverter convert = new UnicodeFileToHtmlTextConverter(new StringReader("H&M"));
+
+        //Act
+
+
+        //Assert
+        Assert.assertEquals("H&amp;M<br />", convert.convertToHtml());
     }
 }
