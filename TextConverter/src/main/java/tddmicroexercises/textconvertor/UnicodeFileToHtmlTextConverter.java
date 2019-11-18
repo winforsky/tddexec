a0836999d89f8b6da4898d21c348786017ef13e4
郑少朋 <911917@nd.com>
2019-11-18 18:48:58 +0800

    添加大于小于换行的实现

--- TextConverter/src/main/java/tddmicroexercises/textconvertor/UnicodeFileToHtmlTextConverter.java
+++ TextConverter/src/main/java/tddmicroexercises/textconvertor/UnicodeFileToHtmlTextConverter.java
@@ -22,13 +22,12 @@ public class UnicodeFileToHtmlTextConverter
     }
 
     //TODO:新增特性：将一个unicode的纯文本字符串转化为HTML编码的字符串
-    //TODO:用户意图：添加转换 & 字符串
-    //TODO:用户意图：添加转换 > < 字符串
-    //TODO:用户意图：添加转换 行分隔符 字符串
+    //TO DO:用户意图：添加转换 & 字符串
+    //TO DO:用户意图：添加转换 > < 字符串
+    //TO DO:用户意图：添加转换 行分隔符 字符串
     public String convertToHtml() throws IOException
     {
-        //TODO:依赖具体实现进行的编程，需要改成面向接口的编程，否则违反依赖倒置和开闭原则
-//
+        //TO DO:依赖具体实现进行的编程，需要改成面向接口的编程，否则违反依赖倒置和开闭原则//
         try (BufferedReader reader = new BufferedReader(this.reader))
             {
 
--- TextConverter/src/test/java/tddmicroexercises/textconvertor/UnicodeFileToHtmlTextConverterTest.java
+++ TextConverter/src/test/java/tddmicroexercises/textconvertor/UnicodeFileToHtmlTextConverterTest.java
@@ -29,4 +29,30 @@ public class UnicodeFileToHtmlTextConverterTest
         //Assert
         Assert.assertEquals("H&amp;M<br />", convert.convertToHtml());
     }
+
+    @Test
+    public void should_convert_greater_or_less() throws IOException {
+
+        //Arrange
+        UnicodeFileToHtmlTextConverter convert = new UnicodeFileToHtmlTextConverter(new StringReader("H>_<M||"));
+
+        //Act
+
+
+        //Assert
+        Assert.assertEquals("H&gt;_&lt;M||<br />", convert.convertToHtml());
+    }
+
+    @Test
+    public void should_convert_line_break() throws IOException {
+
+        //Arrange
+        UnicodeFileToHtmlTextConverter convert = new UnicodeFileToHtmlTextConverter(new StringReader("H\nM||"));
+
+        //Act
+
+
+        //Assert
+        Assert.assertEquals("H<br />M||<br />", convert.convertToHtml());
+    }
 }
