58cfbd58484141de0b3b8d4d92bed3cdbdba0ad5
郑少朋 <911917@nd.com>
2019-11-18 11:31:35 +0800

    修改基于具体实现的todo描述改为基于用户意图的todo描述

--- src/test/java/tddmicroexercises/tirepressuremonitoringsystem/AlarmTest.java
+++ src/test/java/tddmicroexercises/tirepressuremonitoringsystem/AlarmTest.java
@@ -10,6 +10,8 @@ public class AlarmTest {
         assertEquals(6, 2+4);
         //TODO:实现新特性：若前一次检查的胎压值在正常范围之外并保健，后一次检查的胎压值又回到正常范围，此时需要停止报警。
         //TODO:the alarm will be turned off before each checking of pressure
+        //TODO:a normal pressure value after a value outside the range should stop the alarm
+
 
 
     }
