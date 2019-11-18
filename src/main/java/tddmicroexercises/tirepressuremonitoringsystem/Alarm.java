27624a1ff556e82f2d983908f13d4cb99cfc41ee
郑少朋 <911917@nd.com>
2019-11-18 11:35:15 +0800

    发现违反依赖倒置和开闭原则的todo描述

--- src/main/java/tddmicroexercises/tirepressuremonitoringsystem/Alarm.java
+++ src/main/java/tddmicroexercises/tirepressuremonitoringsystem/Alarm.java
@@ -5,6 +5,8 @@ public class Alarm
     private final double lowPressureThreshold = 17;
     private final double highPressureThreshold = 21;
 
+    //TODO:depend on a concrete Sensor violates the Dependency Inversion Principle and Open-Close Principle
+    //TODO:此处依赖具体的Sensor类，违反依赖倒置原则和开闭原则
     private Sensor sensor = new Sensor();
 
     private boolean alarmOn = false;
