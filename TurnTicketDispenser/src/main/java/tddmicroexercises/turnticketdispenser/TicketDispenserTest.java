4c425d05ff3344b8c6667f8477bdfba3b8f434a8
郑少朋 <911917@nd.com>
2019-11-18 15:38:05 +0800

    发现问题，并补充todo特性

---
+++ TurnTicketDispenser/src/main/java/tddmicroexercises/turnticketdispenser/TicketDispenserTest.java
@@ -0,0 +1,10 @@
+package tddmicroexercises.turnticketdispenser;
+
+import static org.junit.Assert.*;
+
+public class TicketDispenserTest {
+
+    @org.junit.Test
+    public void getTurnTicket() {
+    }
+}
