942e61a20de7afa899e5d4b109cae34b41f94bf0
郑少朋 <911917@nd.com>
2019-11-18 15:38:25 +0800

    从用户意图视角补充todo特性

--- TurnTicketDispenser/src/main/java/tddmicroexercises/turnticketdispenser/TicketDispenserTest.java
+++ TurnTicketDispenser/src/main/java/tddmicroexercises/turnticketdispenser/TicketDispenserTest.java
@@ -7,4 +7,9 @@ public class TicketDispenserTest {
     @org.junit.Test
     public void getTurnTicket() {
     }
+
+    //TODO: 新得到的票号 比 之前得到的票号 大
+    //TODO: 新得到的票号 比 另一台取号机上之前得到的票号 大
+    //TODO: 给定取号机11号，那么取号机应该能得到11号
+
 }
