2b7d793eabdeb0cd442a458ec558d666a133afb4
郑少朋 <911917@nd.com>
2019-11-18 14:21:31 +0800

    初始文件

---
+++ TurnTicketDispenser/src/main/java/tddmicroexercises/turnticketdispenser/TicketDispenser.java
@@ -0,0 +1,13 @@
+package tddmicroexercises.turnticketdispenser;
+
+public class TicketDispenser
+{
+
+    public TurnTicket getTurnTicket()
+    {
+        int newTurnNumber = TurnNumberSequence.getNextTurnNumber();
+        TurnTicket newTurnTicket = new TurnTicket(newTurnNumber);
+
+        return newTurnTicket;
+    }
+}
