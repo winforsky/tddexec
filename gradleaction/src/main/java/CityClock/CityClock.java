8e97daa5c7a0cfd6bc51546c7fd417f7ab54be63
郑少朋 <911917@nd.com>
2019-11-20 18:27:30 +0800

    消除重复，完善代码逻辑

--- gradleaction/src/main/java/CityClock/CityClock.java
+++ gradleaction/src/main/java/CityClock/CityClock.java
@@ -2,12 +2,17 @@ package CityClock;
 
 public class CityClock {
     private int utcOffset;
+    private int utcZeroTime;
 
     public CityClock(int utcOffset) {
         this.utcOffset = utcOffset;
     }
 
     public int getTime() {
-        return 1;
+        return this.utcOffset + this.utcZeroTime;
+    }
+
+    public void setUtcZeroTime(int utcZeroTime) {
+        this.utcZeroTime = utcZeroTime;
     }
 }
--- gradleaction/src/main/java/PhoneClock/PhoneClock.java
+++ gradleaction/src/main/java/PhoneClock/PhoneClock.java
@@ -4,6 +4,7 @@ import CityClock.CityClock;
 
 public class PhoneClock {
     private int utcOffset;
+    private CityClock cityClock;
 
     public PhoneClock(int utcOffset) {
 
@@ -12,9 +13,10 @@ public class PhoneClock {
 
     public void setCityClock(CityClock cityClock) {
 
+        this.cityClock = cityClock;
     }
 
     public void setTime(int time) {
-
+        this.cityClock.setUtcZeroTime(time - this.utcOffset);
     }
 }
