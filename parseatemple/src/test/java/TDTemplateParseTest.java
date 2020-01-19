92febd80ee662ae4b61df082ed45805e9eebe86d
郑少朋 <911917@nd.com>
2020-01-19 16:36:59 +0800

    重构测试代码

--- parseatemple/src/test/java/TDTemplateParseTest.java
+++ parseatemple/src/test/java/TDTemplateParseTest.java
@@ -1,6 +1,7 @@
 import org.junit.Assert;
 import org.junit.Test;
 
+import java.util.Arrays;
 import java.util.List;
 
 public class TDTemplateParseTest {
@@ -8,8 +9,13 @@ public class TDTemplateParseTest {
     @Test
     public void emptyTemplateRendersAsEmptyString(){
         List<String> segments = parse("");
-        Assert.assertEquals("Number of segments", 1, segments.size());
-        Assert.assertEquals("", segments.get(0));
+        assertSegments(segments, "");
+    }
+
+    //利用变长参数的语法糖来实现
+    private void assertSegments(List<? extends  Object> actual, Object... expected) {
+        Assert.assertEquals("Number of segments does not match", expected.length, actual.size());
+        Assert.assertEquals(Arrays.asList(expected), actual);
     }
 
     private List<String> parse(String templateText) {
@@ -20,7 +26,6 @@ public class TDTemplateParseTest {
     @Test
     public void templateWithOnlyPlainText(){
         List<String> segments = parse("plain text only");
-        Assert.assertEquals("Number of segments", 1, segments.size());
-        Assert.assertEquals("plain text only", segments.get(0));
+        assertSegments(segments, "plain text only");
     }
 }
