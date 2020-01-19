f6e816f20bc49ea034ec16dc64f7c7b682afaa04
郑少朋 <911917@nd.com>
2020-01-19 14:41:10 +0800

    引入新的测试来去掉另外一个硬编码

--- parseatemple/src/test/java/TempleParseTests.java
+++ parseatemple/src/test/java/TempleParseTests.java
@@ -15,9 +15,19 @@ public class TempleParseTests {
     public void oneVariableWithWriter() throws Exception {
         TDTemplate template = new TDTemplate("Hello, ${name}");
         //用另一个值来做三角定位
+        //去掉一个硬编码代码
         //三角法表示会从多个方向同时入手来确保出恰当的实现
         //三角法有助于防止过早优化、功能蔓延以及总体上的过度设计
         template.set("name", "Writer");
         assertEquals("Hello, Writer", template.evaluate());
+    }
+
+    @Test
+    public void oneVariableTemplateWithReader() throws Exception {
+        //用另一个值来做三角定位
+        //去掉一个硬编码代码
+        TDTemplate template = new TDTemplate("Hi, ${name}");
+        template.set("name", "Reader");
+        assertEquals("Hi, Reader", template.evaluate());
     }
 }
