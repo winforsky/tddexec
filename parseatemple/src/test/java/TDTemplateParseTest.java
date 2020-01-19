360831a51bfab34a52688e8fed5f10e8e2ae1c4d
郑少朋 <911917@nd.com>
2020-01-19 16:39:33 +0800

    新增测试用例

--- parseatemple/src/test/java/TDTemplateParseTest.java
+++ parseatemple/src/test/java/TDTemplateParseTest.java
@@ -12,6 +12,18 @@ public class TDTemplateParseTest {
         assertSegments(segments, "");
     }
 
+    @Test
+    public void templateWithOnlyPlainText(){
+        List<String> segments = parse("plain text only");
+        assertSegments(segments, "plain text only");
+    }
+
+    @Test
+    public void templateWithMultiVariables(){
+        List<String> segments = parse("${a}:${b}:${c}");
+        assertSegments(segments, "${a}", ":", "${b}", ":", "${c}");
+    }
+
     //利用变长参数的语法糖来实现
     private void assertSegments(List<? extends  Object> actual, Object... expected) {
         Assert.assertEquals("Number of segments does not match", expected.length, actual.size());
@@ -23,9 +35,5 @@ public class TDTemplateParseTest {
         return parse.parse(templateText);
     }
 
-    @Test
-    public void templateWithOnlyPlainText(){
-        List<String> segments = parse("plain text only");
-        assertSegments(segments, "plain text only");
-    }
+
 }
