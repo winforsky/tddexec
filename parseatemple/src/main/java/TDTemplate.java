bb134907c955c52357af3c64fd94f0bdab026404
郑少朋 <911917@nd.com>
2020-01-19 17:23:12 +0800

    挖掘进一步的优化点，消除if-else

--- parseatemple/src/main/java/TDTemplate.java
+++ parseatemple/src/main/java/TDTemplate.java
@@ -18,6 +18,23 @@ public class TDTemplate {
         this.variables.put(variable, value);
     }
 
+    /*
+    * 统一处理不同的片段类型
+    * public String evaluate() {
+        TDTemplateParse parse = new TDTemplateParse();
+        List<TDSegment> segments = parse.parse(templateText);
+        return concatenate(segments);
+    }
+
+    private String concatenate(List<TDSegment> segments) {
+        StringBuilder result = new StringBuilder();
+        for (TDSegment segment: segments){
+            segment.append(result, variables);
+        }
+        return result.toString();
+    }
+    * */
+
     public String evaluate() {
         TDTemplateParse parse = new TDTemplateParse();
         List<String> segments = parse.parse(templateText);
